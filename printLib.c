573b16e45073a7c299fc258a9cb434aef6bce28c
Madeleine Johnson (S) <m_johnson2023@login.mcscn.cs.coloradocollege.edu>
2025-05-28 16:43:12 -0600

    updated printer with partial work for sprintf

--- printLib.c
+++ printLib.c
@@ -25,8 +25,8 @@ int printf(const char *format, ...){
 
 	//myPrintf = dlsym(handle, "printf");
 	
-	int (*printf_ptr)(const char *format, ...) = dlsym(handle, "printf");
-	if (!printf_ptr) {
+	int (*sprintf_ptr)(const char *format, ...) = dlsym(handle, "sprintf");
+	if (!sprintf_ptr) {
         	//if symbol can't be found (should be the needed function I'm replacing)
         	fprintf(stderr, "Error: %s\n", dlerror());
         	dlclose(handle);
@@ -37,7 +37,6 @@ int printf(const char *format, ...){
 	
 	//printf_ptr("ok bang");
 
-	dlclose(handle);
 
 	
         //gets the initial time from gettimeofday. tv is the time
@@ -63,11 +62,12 @@ int printf(const char *format, ...){
         char buffer4[50];
 
         //converts time components to strings
-        sprintf(buffer, "%f", hour);
-        sprintf(buffer2, "%f", min);
-        sprintf(buffer3, "%f", sec);
-        sprintf(buffer4, "%f", msec);
-
+        sprintf_ptr(buffer, "%f", hour);
+        sprintf_ptr(buffer2, "%f", min);
+        sprintf_ptr(buffer3, "%f", sec);
+        sprintf_ptr(buffer4, "%f", msec);
+		
+	dlclose(handle);
         //Gets rid of trailing 0s
         int length = strlen(buffer);
         buffer[length-7] = '\0';
@@ -104,4 +104,96 @@ int printf(const char *format, ...){
 }
 
 
+int sprintf(char *buffer, const char *format, ...){
 
+	//sets up dlopen to open the shared library containing print
+        void *handle;
+
+        handle = dlopen("libc.so.6", RTLD_LAZY);
+        if (!handle){
+                //if failed to load the library
+                fprintf(stderr, "Error: %s\n", dlerror());
+                return EXIT_FAILURE;
+        }
+
+        //myPrintf = dlsym(handle, "printf");
+
+        int (*sprintf_ptr)(const char *format, ...) = dlsym(handle, "sprintf");
+        if (!sprintf_ptr) {
+                //if symbol can't be found (should be the needed function I'm replacing)
+                fprintf(stderr, "Error: %s\n", dlerror());
+                dlclose(handle);
+                return EXIT_FAILURE;
+        }
+
+        //uses my printf and then closes the library! Yay!
+
+        //printf_ptr("ok bang");
+
+	printf("ok do that");
+        //gets the initial time from gettimeofday. tv is the time
+        struct timeval tv;
+
+        struct tm *today;
+
+	gettimeofday(&tv, NULL);
+        today = localtime(&tv.tv_sec);
+
+        //gets all needed time components
+        double  hour = today->tm_hour;
+        char buffer[50];
+
+
+        double min = today->tm_min;
+        char buffer2[50];
+
+        double sec = today->tm_sec;
+        char buffer3[50];
+
+        double msec = tv.tv_usec;
+        char buffer4[50];
+
+        //converts time components to strings
+        sprintf_ptr(buffer, "%f", hour);
+        sprintf_ptr(buffer2, "%f", min);
+        sprintf_ptr(buffer3, "%f", sec);
+        sprintf_ptr(buffer4, "%f", msec);
+
+	dlclose(handle);
+
+
+        //Gets rid of trailing 0s
+        int length = strlen(buffer);
+        buffer[length-7] = '\0';
+
+        int length2 = strlen(buffer2);
+        buffer2[length2-7] = '\0';
+
+         int length3 = strlen(buffer3);
+        buffer3[length3-7] = '\0';
+
+        int length4 = strlen(buffer4);
+        buffer4[length4-7] = '\0';
+
+	//concatonates time stamp
+        char stamp[50] = "Time stamp: ";
+        strcat(stamp, buffer);
+        strcat(stamp, ":");
+        strcat(stamp,buffer2);
+        strcat(stamp, ":");
+        strcat(stamp,buffer3);
+        strcat(stamp, ":");
+        strcat(stamp,buffer4);
+        puts(stamp);
+
+        //prints original message
+        va_list args;
+        va_start(args, format);
+        //vprintf(format, args);
+        puts(format);
+        //vprintf("", args);
+        va_end(args);
+
+        return 0;
+
+}
